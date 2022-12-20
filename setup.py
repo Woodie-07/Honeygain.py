d9c2582e8a29d088a7b2866e31ff43afa2b05994
Woodie-07 <40068306+Woodie-07@users.noreply.github.com>
2022-12-20 21:25:05 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='honeygain',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     description='A python library to interact with the Honeygain API',
     author='Woodie',
     url='https://github.com/Woodie-07/honeygain.py',
